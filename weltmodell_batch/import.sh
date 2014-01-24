8c66884261ec6fb19ee25a81e5c7361d11a0abaa
mhuelfen <huelfe@gmx.de>
2014-01-24 15:24:45 +0100

    fixed bugs in convert script
    finished import.sh

--- weltmodell_batch/import.sh
+++ weltmodell_batch/import.sh
@@ -1,3 +1,3 @@
 echo "Run in main directory sh sample/import.sh"
 mvn test-compile exec:java -Dexec.mainClass="org.neo4j.batchimport.Importer" \
-  -Dexec.args="sample/batch.properties target/graph.db sample/nodes.csv,sample/nodes2.csv sample/rels.csv"
+  -Dexec.args="weltmodell_batch/batch.properties target/weltmod_graph.db weltmodell_data/noun_nodes.csv,weltmodell_data/stat_nodes.csv weltmodell_data/noun_sim_rels.csv,weltmodell_data/stat_sim_rels.csv,weltmodell_data/in_state_rels.csv,weltmodell_data/may_be_rels.csv"

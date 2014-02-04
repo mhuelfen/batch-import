a15344a84859945ce749880b853c9715a694e2ca
mhuelfen <huelfe@gmx.de>
2014-02-04 13:42:43 +0100

    creating bidirectional relations for similarities lower cosine distance threshold

--- pre_process_scripts/convert_wm_files_for_batch_input.sh
+++ pre_process_scripts/convert_wm_files_for_batch_input.sh
@@ -1,17 +1,17 @@
-# noun_noun_sims=../../../data/nouns_min_92.csv
-# stat_stat_sims=../../../data/statement_min_92
-# noun_stat=../../../data/noun_state.csv
-# output_dir=../weltmodell_data
+noun_noun_sims=../../../data/nouns_min_92.csv
+stat_stat_sims=../../../data/statement_min_92
+noun_stat=../../../data/noun_state.csv
+output_dir=../weltmodell_data
 
-# test files
-noun_noun_sims=../../../data/test_nn.csv
-stat_stat_sims=../../../data/test_ss.csv
-noun_stat=../../../data/test_ns.csv
-output_dir=../weltmodell_test_data
+# # test files
+# noun_noun_sims=../../../data/test_nn.csv
+# stat_stat_sims=../../../data/test_ss.csv
+# noun_stat=../../../data/test_ns.csv
+# output_dir=../weltmodell_test_data
 
 
 # to filter out to low similarities
-max_cos_dist=0.9
+max_cos_dist=0.88
 
 # add line at file start
 function prepend {
@@ -33,12 +33,12 @@ echo "Finished $output_dir/stat_nodes.csv"
 ### make relationship files
 
 # noun similarity rels
-awk -v mcd=$max_cos_dist 'BEGIN { FS="\t";  OFS="\t";  } { if ($3 <= mcd) print $1,$2,"SIM_NOUN",$3}' $noun_noun_sims > $output_dir/noun_sim_rels.csv
+awk -v mcd=$max_cos_dist 'BEGIN { FS="\t";  OFS="\t";  } { if ($3 <= mcd) print $1,$2,"SIM_NOUN",$3"\n"$2,$1,"SIM_NOUN",$3}' $noun_noun_sims > $output_dir/noun_sim_rels.csv
 prepend 'word:string:nouns\tword:string:nouns\ttype\tcos_dist:float' $output_dir/noun_sim_rels.csv
 echo "Finished $output_dir/noun_sim_rels.csv"
 
 # state similarity rels
-awk -v mcd=$max_cos_dist 'BEGIN { FS="\t";  OFS="\t"} { if ($3 <= mcd) print $1,$2,"SIM_STAT",$3}' $stat_stat_sims  > $output_dir/stat_sim_rels.csv
+awk -v mcd=$max_cos_dist 'BEGIN { FS="\t";  OFS="\t"} { if ($3 <= mcd) print $1,$2,"SIM_STAT",$3"\n"$2,$1,"SIM_STAT",$3}' $stat_stat_sims  > $output_dir/stat_sim_rels.csv
 prepend 'term:string:stats\tterm:string:stats\ttype\tcos_dist:float' $output_dir/stat_sim_rels.csv
 echo "Finished $output_dir/stat_sim_rels.csv"
 
